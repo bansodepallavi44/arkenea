89df53704ad635bbf82d7023d98fd948646b8d01
bansodepallavi44 <bansodepallavi44@gmail.com>
2021-10-12 12:00:38 +0530

    final submmision

---
+++ src/app/app.routing.ts
@@ -0,0 +1,19 @@
+import { Routes } from '@angular/router';
+import { FullComponent } from './layouts/full/full.component';
+export const AppRoutes: Routes = [
+  {
+    path: '',
+    component: FullComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      },
+      {
+        path: 'dashboard',
+        loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)
+      }
+    ]
+  }
+];
---
+++ src/app/dashboard/user/interface/user.ts
@@ -0,0 +1,8 @@
+export interface User{
+    fullName: string;
+    emailAddress: string;
+    gender:string;
+    address:string;
+    dateOfBirth: Date;
+    saveButton:string;
+}
